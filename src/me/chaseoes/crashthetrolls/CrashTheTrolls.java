9df3c23439b115776d4ef587241d07a9f7d083c6
chaseoes <chase.oes@gmail.com>
2012-07-17 17:55:25 -0700

    v1.0

--- src/me/chaseoes/crashthetrolls/CrashTheTrolls.java
+++ src/me/chaseoes/crashthetrolls/CrashTheTrolls.java
@@ -6,15 +6,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CrashTheTrolls extends JavaPlugin {
-	
+
 	public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
 		if (cmnd.getName().equalsIgnoreCase("crash")) {
 			if (cs.hasPermission("crashthetrolls.crash")) {
 				if (strings.length == 1) {
 					Player target = getServer().getPlayer(strings[0]);
 					if (target != null) {
-					target.sendMessage("You're about to crash!§");
-					cs.sendMessage("§aSuccessfully crashed that troll.");
+						target.sendMessage("You're about to crash!§");
+						cs.sendMessage("§aSuccessfully crashed that troll.");
 					} else {
 						cs.sendMessage("§cThat player is not online or could not be found.");
 					}
